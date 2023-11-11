29508851497df0b5cae5ef1e84c5b662d21874ef
Redither <slavonchik68@gmail.com>
2023-11-11 14:43:10 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,16 @@
 import sys
-
+from dotenv import load_dotenv
 import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+   load_dotenv(dotenv_path)
 
 USER = os.environ.get('REG_RU_ID')
 NAME = os.environ.get('HOST_VENV_NAME')
 
 INTERP = os.path.expanduser("/var/www/" + USER + "/data/" + NAME + "/bin/python")
+print(INTERP)
 if sys.executable != INTERP:
    os.execl(INTERP, INTERP, *sys.argv)
 
